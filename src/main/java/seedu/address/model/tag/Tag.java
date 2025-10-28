b10e3ad9fdcacf9a464bcb3839ddded52db4e0f0
Reineil <fish82ee@gmail.com>
2025-10-28 21:47:16 +0800

    Reject slash in the regex

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -15,7 +15,7 @@ public class Tag {
     public static final String MESSAGE_CONSTRAINTS =
             "Tag names should only contain alphanumeric characters, spaces and must be at most "
                     + MAX_LENGTH + " characters. Multiple spaces between words will be standardized";
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}/]+(?: [\\p{Alnum}/]+)*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}]+(?: [\\p{Alnum}]+)*";
 
     private static final Pattern MULTIPLE_SPACES_PATTERN = Pattern.compile(" +");
 
