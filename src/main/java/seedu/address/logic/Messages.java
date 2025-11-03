0e6d0bc22627d6ed8c79aad73746b593dc6e2e92
Reineil <fish82ee@gmail.com>
2025-11-03 16:19:59 +0800

    Update delete message

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -14,8 +14,9 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The contact index provided is invalid";
-    public static final String MESSAGE_INVALID_PERSON_INFO = "The contact information provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The contact index provided is invalid.";
+    public static final String MESSAGE_INVALID_PERSON_INFO = "The contact information provided is invalid. Confirm you are deleting one contact at a time. \nPlease make sure NAME is full name (case insensitive). " +
+            "\nWhen there are multiple contacts of that name shown, you should <delete INDEX>. \nPlease make sure the index is a valid integer.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d contact(s) listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Multiple values specified for the following single-valued field(s): ";
